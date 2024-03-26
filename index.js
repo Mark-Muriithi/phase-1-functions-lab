a1bd40599c887628642a8b61bf838462996d00de
Mark <marl.mw.muriithi@gmail.com>
2024-03-26 11:35:49 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const blockLength = 264; // 1 block = 264 feet
+
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42); // Assuming headquarters is at block 42
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * blockLength;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * blockLength;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        fare = 25;
+    } else {
+        fare = 'cannot travel that far';
+    }
+
+    return fare;
+}
